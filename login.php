6701b94ba2cda705582e7ed0c68bd4bd18d41650
Madhurya713 <madhurya1701@gmail.com>
2021-03-12 10:12:48 +0530

    Initial commit

---
+++ login.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Banking</title>
+	<?php require 'assets/autoloader.php'; ?>
+	<?php require 'assets/function.php'; ?>
+	<?php
+    $con = new mysqli('localhost','root','','mybank');
+    define('bankName', 'MF Bank');
+	
+		$error = "";
+		if (isset($_POST['userLogin']))
+		{
+			$error = "";
+  			$user = $_POST['email'];
+		    $pass = $_POST['password'];
+		   
+		    $result = $con->query("select * from userAccounts where email='$user' AND password='$pass'");
+		    if($result->num_rows>0)
+		    { 
+		      session_start();
+		      $data = $result->fetch_assoc();
+		      $_SESSION['userId']=$data['id'];
+		      $_SESSION['user'] = $data;
+		      header('location:index.php');
+		     }
+		    else
+		    {
+		      $error = "<div class='alert alert-warning text-center rounded-0'>Username or password wrong try again!</div>";
+		    }
+		}
+		if (isset($_POST['cashierLogin']))
+		{
+			$error = "";
+  			$user = $_POST['email'];
+		    $pass = $_POST['password'];
+		   
+		    $result = $con->query("select * from login where email='$user' AND password='$pass'");
+		    if($result->num_rows>0)
+		    { 
+		      session_start();
+		      $data = $result->fetch_assoc();
+		      $_SESSION['cashId']=$data['id'];
+		      //$_SESSION['user'] = $data;
+		      header('location:cindex.php');
+		     }
+		    else
+		    {
+		      $error = "<div class='alert alert-warning text-center rounded-0'>Username or password wrong try again!</div>";
+		    }
+		}
+		if (isset($_POST['managerLogin']))
+		{
+			$error = "";
+  			$user = $_POST['email'];
+		    $pass = $_POST['password'];
+		   
+		    $result = $con->query("select * from login where email='$user' AND password='$pass' AND type='manager'");
+		    if($result->num_rows>0)
+		    { 
+		      session_start();
+		      $data = $result->fetch_assoc();
+		      $_SESSION['managerId']=$data['id'];
+		      //$_SESSION['user'] = $data;
+		      header('location:mindex.php');
+		     }
+		    else
+		    {
+		      $error = "<div class='alert alert-warning text-center rounded-0'>Username or password wrong try again!</div>";
+		    }
+		}
+
+	 ?>
+</head>
+<body style="background: url(images/nature.jpg);background-size: 100%">
+<h1 class="alert alert-secondary" role="alert"><?php echo bankName; ?></h1>
+<h1>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; <b>WELCOME TO MF BANK </b><h1>
+<br>
+<h3>MF Bank is one of the Best public sector banks owned by the Private Company.</h3>
+<p>MF bank is a financial institution licensed to receive deposits and make loans.<Br> MF Banks also provide financial services such as wealth management, currency exchange, and safe deposit boxes.</p>
+<hr>
+<br>
+
+<?php echo $error ?>
+<br>
+<div id="accordion" role="tablist" class="w-25 float-right shadowBlack" style="margin-right: 222px">
+	<br><h4 class="text-center text-white">Select Your Session</h4>
+  <div class="card rounded-0">
+    <div class="card-header" role="tab" id="headingOne">
+      <h5 class="mb-0">
+        <a style="text-decoration: none;" data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+         <button class="btn btn-outline-success btn-block">User Login</button>
+        </a>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+       <form method="POST">
+       	<input type="email" value="Madhurya@gmail.com" name="email" class="form-control" required placeholder="Enter Email">
+       	<input type="password" name="password" value="12345" class="form-control" required placeholder="Enter Password">
+       	<button type="submit" class="btn btn-primary btn-block btn-sm my-1" name="userLogin">Enter </button>
+       </form>
+      </div>
+    </div>
+  </div>
+  <div class="card rounded-0">
+    <div class="card-header" role="tab" id="headingTwo">
+      <h5 class="mb-0">
+        <a class="collapsed btn btn-outline-success btn-block" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Manager Login
+        </a>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+         <form method="POST">
+       	<input type="email" value="Madhurya@manager.com" name="email" class="form-control" required placeholder="Enter Email">
+       	<input type="password" name="password" value="12345" class="form-control" required placeholder="Enter Password">
+       	<button type="submit" class="btn btn-primary btn-block btn-sm my-1" name="managerLogin">Enter </button>
+       </form>
+      </div>
+    </div>
+  </div>
+  <div class="card rounded-0">
+    <div class="card-header" role="tab" id="headingThree">
+      <h5 class="mb-0">
+        <a class="collapsed btn btn-outline-success btn-block" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+         Cashier Login
+        </a>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+        <form method="POST">
+       	<input type="email" value="Madhurya@cashier.com" name="email" class="form-control" required placeholder="Enter Email">
+       	<input type="password" name="password" value="12345" class="form-control" required placeholder="Enter Password">
+       	<button type="submit"  class="btn btn-primary btn-block btn-sm my-1" name="cashierLogin">Enter </button>
+       </form>
+      </div>
+    </div>
+  </div>
+</div>
+<div>
+<h3>Contact us</h3>
+<img src="images/FB-LOGO.png" alt="error loading" width="100" height="100"><br>
+<a href="https://facebook.com"target ="_blank"><b>Facebook</b>></a><br>
+</div>
+<div>
+<img src="images/linkidin.png" alt="error loading" width="100" height="100"><br>
+<a href="https://www.linkedin.com/in/madhurya-kumar-164a8a1aa"target ="_blank"><b>Linkedin</b>></a><br>
+</div>
+<div>
+<img src="images/twitter.png" alt="error loading" width="100" height="100"><br>
+<a href="https://twitter.com"target ="_blank"><b> Twitter</b>></a><br>
+</div>
+<div>
+<img src="images/gmail.png" alt="error loading" width="100" height="100"><br>
+<a href="https://gmail.com"target ="_blank"><b> Gmail</b>></a><br>
+</div>
+
+
+
+</body>
+</html>
